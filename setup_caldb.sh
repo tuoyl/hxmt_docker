5c8e1aeff9fd82edcb32cd8a08fb88be1101300f
Youli Tuo <tuoyl@ihep.ac.cn>
2025-09-25 13:21:40 +0200

    Update setup_caldb.sh
    configure CALDB for version 2.07

--- setup_caldb.sh
+++ setup_caldb.sh
@@ -1,9 +1,8 @@
-#! /bin/sh
+#! /bin/sh                                                                                                                                                                                                                                
+
+export caldbversion=2.07
 
 cd $ASTROSOFT/hxmtsoft/
-echo `ls`
-wget -O CALDB.tar.gz  http://www.hxmt.cn/u/cms/www/202201/CALDB2.06.tar.gz
-echo `ls`
-gunzip CALDB.tar.gz
-tar -xvf CALDB.tar
-mv CALDB2.06 CALDB
+gunzip CALDB${caldbversion}.tar.gz
+tar -xvf CALDB${caldbversion}.tar
+mv CALDB${caldbversion} CALDB
