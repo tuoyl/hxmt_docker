19e6befb03821cd9d02230cbe53d017f0c09f595
Youli Tuo <tuoyl@ihep.ac.cn>
2025-09-25 13:23:14 +0200

    Update setup_hxmtsoft.sh
    update the installation for hxmtsoft

--- setup_hxmtsoft.sh
+++ setup_hxmtsoft.sh
@@ -1,30 +1,24 @@
 #! /bin/sh
 
 cd $ASTROSOFT/hxmtsoft/source/
-export hversion=2.05
+export hversion=2.06
 echo `pwd`
 echo `ls`
 
 # careful about v_2.04 or v2.04
-wget -q -O hxmtsoftv${hversion}.tar.gz http://www.hxmt.cn/u/cms/www/202201/hxmtsoftv2.05.tar.gz
 echo `ls`
 gunzip hxmtsoftv${hversion}.tar.gz
 tar -xvf hxmtsoftv${hversion}.tar
 cd $ASTROSOFT/hxmtsoft/source/hxmtsoftv${hversion}/BUILD_DIR/
-#cd $ASTROSOFT/hxmtsoft/source/BUILD_DIR/
 
 make clean
 make distclean
 
-# fix gcc-gfortran version too old problem
-yum -y install centos-release-scl
-yum -y  install devtoolset-7-gcc*
-scl enable devtoolset-7 bash
+export CC=$(command -v gcc)
+export CXX=$(command -v g++)
+export FC=$(command -v gfortran)
 
-export CC=/opt/rh/devtoolset-7/root/usr/bin/gcc
-export CXX=/opt/rh/devtoolset-7/root/usr/bin/g++
-export FC=/opt/rh/devtoolset-7/root/usr/bin/gfortran
-export PERL=/usr/bin/perl
-./configure --prefix=$ASTROSOFT/hxmtsoft/install/ --with-components="heacore tcltk attitude heasptools heatools heagen demo hxmt Xspec ftools "
+./configure --prefix=$ASTROSOFT/hxmtsoft/install/ --with-components="hxmt ftools "
+make -j 4
 make
 make install
